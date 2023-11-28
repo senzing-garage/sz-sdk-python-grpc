52aa6cb46267e885245cedd1e3d5972604e2ff88
docktermj <michael@dockter.com>
2023-11-28 12:02:04 -0500

    #3 Working tests

--- tests/g2config_test.py
+++ tests/g2config_test.py
@@ -374,161 +374,161 @@ def test_add_data_source(g2_config):
     assert schema(add_data_source_schema) == actual_json
 
 
-# def test_add_data_source_dict(g2_config):
-#     """Test G2Config().add_data_source()."""
-#     input_json_dict = {"DSRC_CODE": "NAME_OF_DATASOURCE"}
-#     config_handle = g2_config.create()
-#     actual = g2_config.add_data_source(config_handle, input_json_dict)
-#     g2_config.close(config_handle)
-#     assert isinstance(actual, str)
-#     actual_json = json.loads(actual)
-#     assert schema(add_data_source_schema) == actual_json
+def test_add_data_source_dict(g2_config):
+    """Test G2Config().add_data_source()."""
+    input_json_dict = {"DSRC_CODE": "NAME_OF_DATASOURCE"}
+    config_handle = g2_config.create()
+    actual = g2_config.add_data_source(config_handle, input_json_dict)
+    g2_config.close(config_handle)
+    assert isinstance(actual, str)
+    actual_json = json.loads(actual)
+    assert schema(add_data_source_schema) == actual_json
 
 
-# def test_add_data_source_bad_config_handle(g2_config):
-#     """Test G2Config().add_data_source()."""
-#     bad_config_handle = "string"
-#     input_json_dict = {"DSRC_CODE": "NAME_OF_DATASOURCE"}
-#     with pytest.raises(TypeError):
-#         g2_config.add_data_source(bad_config_handle, json.dumps(input_json_dict))
+def test_add_data_source_bad_config_handle(g2_config):
+    """Test G2Config().add_data_source()."""
+    bad_config_handle = "string"
+    input_json_dict = {"DSRC_CODE": "NAME_OF_DATASOURCE"}
+    with pytest.raises(TypeError):
+        g2_config.add_data_source(bad_config_handle, json.dumps(input_json_dict))
 
 
-# def test_add_data_source_bad_input_json(g2_config):
-#     """Test G2Config().add_data_source()."""
-#     config_handle = g2_config.create()
-#     bad_input_json = 0
-#     try:
-#         with pytest.raises(TypeError):
-#             g2_config.add_data_source(config_handle, bad_input_json)
-#     finally:
-#         g2_config.close(config_handle)
+def test_add_data_source_bad_input_json(g2_config):
+    """Test G2Config().add_data_source()."""
+    config_handle = g2_config.create()
+    bad_input_json = 0
+    try:
+        with pytest.raises(TypeError):
+            g2_config.add_data_source(config_handle, bad_input_json)
+    finally:
+        g2_config.close(config_handle)
 
 
-# def test_close_bad_config_handle(g2_config):
-#     """Test G2Config().create()."""
-#     bad_config_handle = "string"
-#     with pytest.raises(TypeError):
-#         g2_config.close(bad_config_handle)
+def test_close_bad_config_handle(g2_config):
+    """Test G2Config().create()."""
+    bad_config_handle = "string"
+    with pytest.raises(TypeError):
+        g2_config.close(bad_config_handle)
 
 
-# def test_create(g2_config):
-#     """Test G2Config().create()."""
-#     config_handle = g2_config.create()
-#     assert isinstance(config_handle, int)
-#     assert config_handle > 0
-#     g2_config.close(config_handle)
-#     assert isinstance(config_handle, int)
-#     assert config_handle > 0
+def test_create(g2_config):
+    """Test G2Config().create()."""
+    config_handle = g2_config.create()
+    assert isinstance(config_handle, int)
+    assert config_handle > 0
+    g2_config.close(config_handle)
+    assert isinstance(config_handle, int)
+    assert config_handle > 0
 
 
-# def test_delete_data_source(g2_config):
-#     """Test G2Config().delete_data_source()."""
-#     input_json_dict = {"DSRC_CODE": "TEST"}
-#     config_handle = g2_config.create()
-#     g2_config.delete_data_source(config_handle, json.dumps(input_json_dict))
-#     g2_config.close(config_handle)
+def test_delete_data_source(g2_config):
+    """Test G2Config().delete_data_source()."""
+    input_json_dict = {"DSRC_CODE": "TEST"}
+    config_handle = g2_config.create()
+    g2_config.delete_data_source(config_handle, json.dumps(input_json_dict))
+    g2_config.close(config_handle)
 
 
-# def test_delete_data_source_dict(g2_config):
-#     """Test G2Config().delete_data_source()."""
-#     input_json_dict = {"DSRC_CODE": "TEST"}
-#     config_handle = g2_config.create()
-#     g2_config.delete_data_source(config_handle, input_json_dict)
-#     g2_config.close(config_handle)
+def test_delete_data_source_dict(g2_config):
+    """Test G2Config().delete_data_source()."""
+    input_json_dict = {"DSRC_CODE": "TEST"}
+    config_handle = g2_config.create()
+    g2_config.delete_data_source(config_handle, input_json_dict)
+    g2_config.close(config_handle)
 
 
-# def test_delete_data_source_bad_config_handle(g2_config):
-#     """Test G2Config().delete_data_source()."""
-#     input_json_dict = {"DSRC_CODE": "TEST"}
-#     bad_config_handle = "string"
-#     with pytest.raises(TypeError):
-#         g2_config.delete_data_source(bad_config_handle, json.dumps(input_json_dict))
+def test_delete_data_source_bad_config_handle(g2_config):
+    """Test G2Config().delete_data_source()."""
+    input_json_dict = {"DSRC_CODE": "TEST"}
+    bad_config_handle = "string"
+    with pytest.raises(TypeError):
+        g2_config.delete_data_source(bad_config_handle, json.dumps(input_json_dict))
 
 
-# def test_delete_data_source_bad_input_json(g2_config):
-#     """Test G2Config().delete_data_source()."""
-#     bad_input_json = 0
-#     config_handle = g2_config.create()
-#     with pytest.raises(TypeError):
-#         g2_config.delete_data_source(config_handle, bad_input_json)
-#     g2_config.close(config_handle)
+def test_delete_data_source_bad_input_json(g2_config):
+    """Test G2Config().delete_data_source()."""
+    bad_input_json = 0
+    config_handle = g2_config.create()
+    with pytest.raises(TypeError):
+        g2_config.delete_data_source(config_handle, bad_input_json)
+    g2_config.close(config_handle)
 
 
-# def test_list_data_sources(g2_config):
-#     """Test G2Config().list_data_sources()."""
-#     config_handle = g2_config.create()
-#     actual = g2_config.list_data_sources(config_handle)
-#     g2_config.close(config_handle)
-#     assert isinstance(actual, str)
-#     actual_json = json.loads(actual)
-#     assert schema(list_data_sources_schema) == actual_json
+def test_list_data_sources(g2_config):
+    """Test G2Config().list_data_sources()."""
+    config_handle = g2_config.create()
+    actual = g2_config.list_data_sources(config_handle)
+    g2_config.close(config_handle)
+    assert isinstance(actual, str)
+    actual_json = json.loads(actual)
+    assert schema(list_data_sources_schema) == actual_json
 
 
-# def test_list_data_sources_bad_config_handle(g2_config):
-#     """Test G2Config().list_data_sources()."""
-#     bad_config_handle = "string"
-#     with pytest.raises(TypeError):
-#         g2_config.list_data_sources(bad_config_handle)
+def test_list_data_sources_bad_config_handle(g2_config):
+    """Test G2Config().list_data_sources()."""
+    bad_config_handle = "string"
+    with pytest.raises(TypeError):
+        g2_config.list_data_sources(bad_config_handle)
 
 
-# def test_load(g2_config):
-#     """Test G2Config().load()."""
-#     config_handle = g2_config.create()
-#     json_config = g2_config.save(config_handle)
-#     config_handle = g2_config.load(json_config)
-#     assert isinstance(config_handle, int)
-#     assert config_handle > 0
-#     g2_config.close(config_handle)
+def test_load(g2_config):
+    """Test G2Config().load()."""
+    config_handle = g2_config.create()
+    json_config = g2_config.save(config_handle)
+    config_handle = g2_config.load(json_config)
+    assert isinstance(config_handle, int)
+    assert config_handle > 0
+    g2_config.close(config_handle)
 
 
-# def test_load_dict(g2_config):
-#     """Test G2Config().load()."""
-#     config_handle = g2_config.create()
-#     json_config = g2_config.save(config_handle)
-#     json_config_dict = json.loads(json_config)
-#     config_handle = g2_config.load(json_config_dict)
-#     assert isinstance(config_handle, int)
-#     assert config_handle > 0
-#     g2_config.close(config_handle)
+def test_load_dict(g2_config):
+    """Test G2Config().load()."""
+    config_handle = g2_config.create()
+    json_config = g2_config.save(config_handle)
+    json_config_dict = json.loads(json_config)
+    config_handle = g2_config.load(json_config_dict)
+    assert isinstance(config_handle, int)
+    assert config_handle > 0
+    g2_config.close(config_handle)
 
 
-# def test_load_bad_json_config(g2_config):
-#     """Test G2Config().load()."""
-#     bad_json_config = 0
-#     with pytest.raises(TypeError):
-#         g2_config.load(bad_json_config)
+def test_load_bad_json_config(g2_config):
+    """Test G2Config().load()."""
+    bad_json_config = 0
+    with pytest.raises(TypeError):
+        g2_config.load(bad_json_config)
 
 
-# def test_save(g2_config):
-#     """Test G2Config().save()."""
-#     config_handle = g2_config.create()
-#     actual = g2_config.save(config_handle)
-#     g2_config.close(config_handle)
-#     assert isinstance(actual, str)
-#     actual_json = json.loads(actual)
-#     assert schema(save_schema) == actual_json
+def test_save(g2_config):
+    """Test G2Config().save()."""
+    config_handle = g2_config.create()
+    actual = g2_config.save(config_handle)
+    g2_config.close(config_handle)
+    assert isinstance(actual, str)
+    actual_json = json.loads(actual)
+    assert schema(save_schema) == actual_json
 
 
-# def test_save_bad_config_handle(g2_config):
-#     """Test G2Config().save()."""
-#     bad_config_handle = "string"
-#     with pytest.raises(TypeError):
-#         g2_config.save(bad_config_handle)
+def test_save_bad_config_handle(g2_config):
+    """Test G2Config().save()."""
+    bad_config_handle = "string"
+    with pytest.raises(TypeError):
+        g2_config.save(bad_config_handle)
 
 
-# def test_init_and_destroy(g2_config):
-#     """Test G2Config().init() and G2Config.destroy()."""
-#     g2_config.init("Example", "{}", 0)
-#     g2_config.destroy()
+def test_init_and_destroy(g2_config):
+    """Test G2Config().init() and G2Config.destroy()."""
+    g2_config.init("Example", "{}", 0)
+    g2_config.destroy()
 
 
-# def test_init_and_destroy_dict(g2_config):
-#     """Test G2Config().init() and G2Config.destroy()."""
-#     g2_config.init("Example", {}, 0)
-#     g2_config.destroy()
+def test_init_and_destroy_dict(g2_config):
+    """Test G2Config().init() and G2Config.destroy()."""
+    g2_config.init("Example", {}, 0)
+    g2_config.destroy()
 
 
-# def test_init_and_destroy_again(g2_config):
-#     """Test G2Config().init() and G2Config.destroy()."""
-#     g2_config.init("Example", "{}", 0)
-#     g2_config.destroy()
+def test_init_and_destroy_again(g2_config):
+    """Test G2Config().init() and G2Config.destroy()."""
+    g2_config.init("Example", "{}", 0)
+    g2_config.destroy()
