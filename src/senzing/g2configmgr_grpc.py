bd5905370ae5b930212f4cc500e0db11fa6cad2a
docktermj <michael@dockter.com>
2023-11-28 18:01:13 -0500

    #3 Savepoint

--- src/senzing/g2configmgr_grpc.py
+++ src/senzing/g2configmgr_grpc.py
@@ -4,32 +4,23 @@
 TODO: g2configmgr_grpc.py
 """
 
-# Import from standard library. https://docs.python.org/3/library/
+# pylint: disable=E1101
 
 from typing import Any, Dict, Union
 
+import grpc  # type: ignore
+
 # from .g2exception import translate_exception
-from .g2configmgr_abstract import G2ConfigMgrAbstract
 from .g2helpers import as_str
-
-# from ctypes import *
-# import functools
-# import json
-# import os
-# import threading
-# import warnings
-
-# Import from https://pypi.org/
-
-# Import from Senzing.
-
+from .pb2_grpc import g2configmgr_pb2, g2configmgr_pb2_grpc
+from .tmp.g2configmgr_abstract import G2ConfigMgrAbstract
 
 # Metadata
 
 __all__ = ["G2ConfigMgrGrpc"]
 __version__ = "0.0.1"  # See https://www.python.org/dev/peps/pep-0396/
-__date__ = "2023-10-30"
-__updated__ = "2023-10-30"
+__date__ = "2023-11-27"
+__updated__ = "2023-11-27"
 
 SENZING_PRODUCT_ID = "5051"  # See https://github.com/Senzing/knowledge-base/blob/main/lists/senzing-component-ids.md
 
@@ -49,33 +40,16 @@ class G2ConfigMgrGrpc(G2ConfigMgrAbstract):
 
     def __init__(
         self,
-        module_name: str = "",
-        ini_params: Union[str, Dict[Any, Any]] = "",
-        init_config_id: int = 0,
-        verbose_logging: int = 0,
-        **kwargs: Any,
+        grpc_channel: grpc.Channel,
     ) -> None:
         """
         Constructor
 
         For return value of -> None, see https://peps.python.org/pep-0484/#the-meaning-of-annotations
         """
-        # pylint: disable=W0613
 
-        self.ini_params = as_str(ini_params)
-        self.init_config_id = init_config_id
-        self.module_name = module_name
-        self.noop = ""
-        self.verbose_logging = verbose_logging
-
-    # -------------------------------------------------------------------------
-    # Development methods - to be removed after initial development
-    # -------------------------------------------------------------------------
-
-    def fake_g2configmgr(self, *args: Any, **kwargs: Any) -> None:
-        """TODO: Remove once SDK methods have been implemented."""
-        if len(args) + len(kwargs) > 2000:
-            print(self.noop)
+        self.channel = grpc_channel
+        self.stub = g2configmgr_pb2_grpc.G2ConfigMgrStub(self.channel)
 
     # -------------------------------------------------------------------------
     # G2ConfigMgr methods
@@ -88,23 +62,29 @@ class G2ConfigMgrGrpc(G2ConfigMgrAbstract):
         *args: Any,
         **kwargs: Any,
     ) -> int:
-        self.fake_g2configmgr(config_str, config_comments)
-        return 0
+        request = g2configmgr_pb2.AddDataSourceRequest(
+            configStr=config_str, configComments=as_str(config_str)
+        )
+        result = self.stub.AddConfig(request)
+        return int(result.result)
 
     def destroy(self, *args: Any, **kwargs: Any) -> None:
-        self.fake_g2configmgr()
+        """No-op"""
 
     def get_config(self, config_id: int, *args: Any, **kwargs: Any) -> str:
-        self.fake_g2configmgr(config_id)
-        return "string"
+        request = g2configmgr_pb2.GetConfigRequest(configID=config_id)
+        result = self.stub.GetConfig(request)
+        return str(result.result)
 
     def get_config_list(self, *args: Any, **kwargs: Any) -> str:
-        self.fake_g2configmgr()
-        return "string"
+        request = g2configmgr_pb2.GetConfigListRequest()
+        result = self.stub.GetConfigList(request)
+        return str(result.result)
 
     def get_default_config_id(self, *args: Any, **kwargs: Any) -> int:
-        self.fake_g2configmgr()
-        return 0
+        request = g2configmgr_pb2.GetDefaultConfigIDRequest()
+        result = self.stub.GetDefaultConfigID(request)
+        return int(result.result)
 
     def init(
         self,
@@ -113,12 +93,19 @@ class G2ConfigMgrGrpc(G2ConfigMgrAbstract):
         verbose_logging: int = 0,
         **kwargs: Any,
     ) -> None:
-        self.fake_g2configmgr(module_name, ini_params, verbose_logging)
+        """No-op"""
 
     def replace_default_config_id(
         self, old_config_id: int, new_config_id: int, *args: Any, **kwargs: Any
     ) -> None:
-        self.fake_g2configmgr(old_config_id, new_config_id)
+        request = g2configmgr_pb2.ReplaceDefaultConfigIDRequest(
+            oldConfigID=old_config_id,
+            newConfigID=new_config_id,
+        )
+        self.stub.ReplaceDefaultConfigID(request)
 
     def set_default_config_id(self, config_id: int, *args: Any, **kwargs: Any) -> None:
-        self.fake_g2configmgr(config_id)
+        request = g2configmgr_pb2.SetDefaultConfigIDRequest(
+            configID=config_id,
+        )
+        self.stub.SetDefaultConfigID(request)
