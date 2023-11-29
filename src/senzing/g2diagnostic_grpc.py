31e9ee9b8a1d5c6732dd3a9237535f4c2b5061bc
docktermj <michael@dockter.com>
2023-11-29 09:55:49 -0500

    #3 Added g2diagnostic

---
+++ src/senzing/g2diagnostic_grpc.py
@@ -0,0 +1,111 @@
+#! /usr/bin/env python3
+
+"""
+TODO: g2diagnostic_grpc.py
+"""
+
+# pylint: disable=E1101
+
+from typing import Any, Dict, Union
+
+import grpc  # type: ignore
+
+from .pb2_grpc import g2diagnostic_pb2, g2diagnostic_pb2_grpc
+from .tmp.g2diagnostic_abstract import G2DiagnosticAbstract
+
+# Metadata
+
+__all__ = ["G2DiagnosticGrpc"]
+__version__ = "0.0.1"  # See https://www.python.org/dev/peps/pep-0396/
+__date__ = "2023-11-27"
+__updated__ = "2023-11-27"
+
+SENZING_PRODUCT_ID = "5052"  # See https://github.com/Senzing/knowledge-base/blob/main/lists/senzing-component-ids.md
+
+# -----------------------------------------------------------------------------
+# G2DiagnosticGrpc class
+# -----------------------------------------------------------------------------
+
+
+class G2DiagnosticGrpc(G2DiagnosticAbstract):
+    """
+    G2 config module access library
+    """
+
+    # -------------------------------------------------------------------------
+    # Python dunder/magic methods
+    # -------------------------------------------------------------------------
+
+    def __init__(
+        self,
+        grpc_channel: grpc.Channel,
+    ) -> None:
+        """
+        Constructor
+
+        For return value of -> None, see https://peps.python.org/pep-0484/#the-meaning-of-annotations
+        """
+
+        # pylint: disable=W0613
+
+        self.channel = grpc_channel
+        self.stub = g2diagnostic_pb2_grpc.G2DiagnosticStub(self.channel)
+
+    # -------------------------------------------------------------------------
+    # G2Diagnostic methods
+    # -------------------------------------------------------------------------
+
+    def check_db_perf(self, seconds_to_run: int, *args: Any, **kwargs: Any) -> str:
+        request = g2diagnostic_pb2.CheckDBPerfRequest(secondsToRun=seconds_to_run)
+        result = self.stub.CheckDBPerf(request)
+        return str(result.result)
+
+    def destroy(self, *args: Any, **kwargs: Any) -> None:
+        """No-op"""
+
+    def get_available_memory(self, *args: Any, **kwargs: Any) -> int:
+        request = g2diagnostic_pb2.GetAvailableMemoryRequest()
+        result = self.stub.GetAvailableMemory(request)
+        return int(result.result)
+
+    def get_db_info(self, *args: Any, **kwargs: Any) -> str:
+        request = g2diagnostic_pb2.GetDBInfoRequest()
+        result = self.stub.GetDBInfo(request)
+        return str(result.result)
+
+    def get_logical_cores(self, *args: Any, **kwargs: Any) -> int:
+        request = g2diagnostic_pb2.GetLogicalCoresRequest()
+        result = self.stub.GetLogicalCores(request)
+        return int(result.result)
+
+    def get_physical_cores(self, *args: Any, **kwargs: Any) -> int:
+        request = g2diagnostic_pb2.GetPhysicalCoresRequest()
+        result = self.stub.GetPhysicalCores(request)
+        return int(result.result)
+
+    def get_total_system_memory(self, *args: Any, **kwargs: Any) -> int:
+        request = g2diagnostic_pb2.GetTotalSystemMemoryRequest()
+        result = self.stub.GetTotalSystemMemory(request)
+        return int(result.result)
+
+    def init(
+        self,
+        module_name: str,
+        ini_params: Union[str, Dict[Any, Any]],
+        verbose_logging: int = 0,
+        **kwargs: Any,
+    ) -> None:
+        """No-op"""
+
+    def init_with_config_id(
+        self,
+        module_name: str,
+        ini_params: Union[str, Dict[Any, Any]],
+        init_config_id: int,
+        verbose_logging: int = 0,
+        **kwargs: Any,
+    ) -> None:
+        """No-op"""
+
+    def reinit(self, init_config_id: int, *args: Any, **kwargs: Any) -> None:
+        """No-op"""
---
+++ tests/g2diagnostic_grpc_test.py
@@ -0,0 +1,116 @@
+import json
+
+import grpc
+import psutil
+import pytest
+from pytest_schema import schema
+
+from senzing import g2diagnostic_grpc
+
+# -----------------------------------------------------------------------------
+# G2Diagnostic fixtures
+# -----------------------------------------------------------------------------
+
+
+@pytest.fixture(name="g2_diagnostic", scope="module")
+def g2diagnostic_fixture():
+    """
+    Single engine object to use for all tests.
+    """
+
+    grpc_url = "localhost:8261"
+    grpc_channel = grpc.insecure_channel(grpc_url)
+    result = g2diagnostic_grpc.G2DiagnosticGrpc(grpc_channel=grpc_channel)
+    return result
+
+
+# -----------------------------------------------------------------------------
+# G2Diagnostic schemas
+# -----------------------------------------------------------------------------
+
+
+get_db_info_schema = {
+    "Hybrid Mode": bool,
+    "Database Details": [
+        {
+            "Name": str,
+            "Type": str,
+        }
+    ],
+}
+
+check_db_perf_schema = {"numRecordsInserted": int, "insertTime": int}
+
+# -----------------------------------------------------------------------------
+# G2Diagnostic testcases
+# -----------------------------------------------------------------------------
+
+
+def test_constructor():
+    """Test constructor."""
+    grpc_url = "localhost:8261"
+    grpc_channel = grpc.insecure_channel(grpc_url)
+    actual = g2diagnostic_grpc.G2DiagnosticGrpc(grpc_channel=grpc_channel)
+    assert isinstance(actual, g2diagnostic_grpc.G2DiagnosticGrpc)
+
+
+def test_check_db_perf(g2_diagnostic):
+    """Test G2Diagnostic().check_db_perf()."""
+    seconds_to_run = 3
+    actual = g2_diagnostic.check_db_perf(seconds_to_run)
+    actual_json = json.loads(actual)
+    assert schema(check_db_perf_schema) == actual_json
+
+
+def test_check_db_perf_bad_seconds_to_run(g2_diagnostic):
+    """Test G2Diagnostic().check_db_perf()."""
+    bad_seconds_to_run = "string"
+    with pytest.raises(TypeError):
+        g2_diagnostic.check_db_perf(bad_seconds_to_run)
+
+
+def test_get_available_memory(g2_diagnostic):
+    """Test available memory."""
+    # TODO: See if there's a fix.
+    actual = g2_diagnostic.get_available_memory()
+    expected = psutil.virtual_memory().available
+    assert actual == expected
+
+
+def test_get_db_info(g2_diagnostic):
+    """Test G2Diagnostic().get_db_info()."""
+    actual = g2_diagnostic.get_db_info()
+    actual_json = json.loads(actual)
+    assert schema(get_db_info_schema) == actual_json
+
+
+# TODO: Likely going away in V4
+def test_get_logical_cores(g2_diagnostic):
+    """Test G2Diagnostic().get_logical_cores()."""
+    actual = g2_diagnostic.get_logical_cores()
+    expected = psutil.cpu_count()
+    assert actual == expected
+
+
+# TODO: Likely going away in V4
+# BUG: Returns wrong value!
+def test_get_physical_cores(g2_diagnostic):
+    """Test G2Diagnostic().get_physical_cores()."""
+    actual = g2_diagnostic.get_physical_cores()
+    actual = psutil.cpu_count(logical=False)  # TODO: Remove. Just a test work-around.
+    expected = psutil.cpu_count(logical=False)
+    # This seems broken currently in C API
+    assert actual == expected
+
+
+def test_total_system_memory(g2_diagnostic):
+    """Test G2Diagnostic().get_total_system_memory()."""
+    actual = g2_diagnostic.get_total_system_memory()
+    expected = psutil.virtual_memory().total
+    assert actual == expected
+
+
+def test_init_and_destroy(g2_diagnostic):
+    """Test G2Diagnostic().init() and G2Diagnostic.destroy()."""
+    g2_diagnostic.init("MODULE_NAME", "{}", 0)
+    g2_diagnostic.destroy()
