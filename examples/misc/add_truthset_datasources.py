de7fd66c013d392e8ca5c6d148c466024fbde31c
docktermj <michael@dockter.com>
2023-12-18 11:19:02 -0500

    #3 Update version of senzing-abstract

--- examples/misc/add_truthset_datasources.py
+++ examples/misc/add_truthset_datasources.py
@@ -16,7 +16,6 @@ try:
     grpc_channel = grpc.insecure_channel(GRPC_URL)
     g2_config = G2ConfigGrpc(grpc_channel=grpc_channel)
     g2_configmgr = G2ConfigMgrGrpc(grpc_channel=grpc_channel)
-
     old_config_id = g2_configmgr.get_default_config_id()
     OLD_JSON_CONFIG = g2_configmgr.get_config(old_config_id)
     config_handle = g2_config.load(OLD_JSON_CONFIG)
@@ -25,10 +24,8 @@ try:
     NEW_JSON_CONFIG = g2_config.save(config_handle)
     new_config_id = g2_configmgr.add_config(NEW_JSON_CONFIG, "Add TruthSet datasources")
     g2_configmgr.replace_default_config_id(old_config_id, new_config_id)
-
     g2_engine = G2EngineGrpc(grpc_channel=grpc_channel)
     g2_engine.reinit(new_config_id)
-
     g2_diagnostic = G2DiagnosticGrpc(grpc_channel=grpc_channel)
     g2_diagnostic.reinit(new_config_id)
 except G2Exception as err:
