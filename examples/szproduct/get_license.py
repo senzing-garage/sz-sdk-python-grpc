54c635b9853b394ce99596f92cc8abec645475ac
docktermj <michael@dockter.com>
2024-04-25 09:29:24 -0400

    #43 Savepoint

--- examples/szproduct/get_license.py
+++ examples/szproduct/get_license.py
@@ -2,13 +2,13 @@
 
 import grpc
 
-from senzing_grpc import G2Exception, g2product_grpc
+from senzing_grpc import SzError, szproduct_grpc
 
 try:
     GRPC_URL = "localhost:8261"
     grpc_channel = grpc.insecure_channel(GRPC_URL)
-    g2_product = g2product_grpc.SzProductGrpc(grpc_channel=grpc_channel)
-    RESULT = g2_product.license()
+    sz_product = szproduct_grpc.SzProductGrpc(grpc_channel=grpc_channel)
+    RESULT = sz_product.get_license()
     print(RESULT)
-except G2Exception as err:
+except SzError as err:
     print(f"\nError:\n{err}\n")
--- examples/szproduct/szproduct_initialize_and_destroy.py
+++ examples/szproduct/szproduct_initialize_and_destroy.py
@@ -2,15 +2,15 @@
 
 import grpc
 
-from senzing_grpc import G2Exception, g2product_grpc
+from senzing_grpc import SzError, szproduct_grpc
 
 try:
     GRPC_URL = "localhost:8261"
     grpc_channel = grpc.insecure_channel(GRPC_URL)
-    g2_product = g2product_grpc.SzProductGrpc(grpc_channel=grpc_channel)
+    sz_product = szproduct_grpc.SzProductGrpc(grpc_channel=grpc_channel)
 
     # Do work.
 
-    g2_product.destroy()
-except G2Exception as err:
+    sz_product.destroy()
+except SzError as err:
     print(f"\nError:\n{err}\n")
