8a15a6bd81a223b4c5760bc695a858ab78ce8749
docktermj <michael@dockter.com>
2024-04-29 10:15:42 -0400

    #43 Update documentation

--- examples/szconfig/import_config.py
+++ examples/szconfig/import_config.py
@@ -1,17 +1,25 @@
 #! /usr/bin/env python3
 
-from typing import Any, Dict
 
 import grpc
 
-from senzing_grpc import SzError, szconfig_grpc
+from senzing_grpc import SzError, szconfig_grpc, szconfigmanager_grpc
 
-# This would be a full Senzing configuration.
-config_definition: Dict[str, Any] = {}  # In this example, an exception occurs.
 GRPC_URL = "localhost:8261"
 
 try:
     grpc_channel = grpc.insecure_channel(GRPC_URL)
+
+    # For this example, get default configuration.
+
+    sz_configmanager = szconfigmanager_grpc.SzConfigManagerGrpc(
+        grpc_channel=grpc_channel
+    )
+    config_id = sz_configmanager.get_default_config_id()
+    config_definition = sz_configmanager.get_config(config_id)
+
+    # Import the configuration.
+
     sz_config = szconfig_grpc.SzConfigGrpc(grpc_channel=grpc_channel)
     config_handle = sz_config.import_config(config_definition)
 except SzError as err:
