02e7c5593a8f26c3d1fe81ec9cf4db07aaca5248
Michael Dockter <michael@dockter.com>
2024-08-12 15:01:03 -0400

    #67 Add type: ignore[misc]

--- src/senzing_grpc/szproduct.py
+++ src/senzing_grpc/szproduct.py
@@ -29,7 +29,7 @@ SENZING_PRODUCT_ID = "5056"  # See https://github.com/senzing-garage/knowledge-b
 # -----------------------------------------------------------------------------
 
 
-class SzProduct(SzProductAbstract):
+class SzProduct(SzProductAbstract):  # type: ignore[misc]
     """
     SzProduct module access library over gRPC.
     """
