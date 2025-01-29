b1b8a834b848ed5cb337053d31ebeca686683471
docktermj <michael@dockter.com>
2025-01-29 15:37:06 -0500

    #194 Savepoint

--- examples/szengine/export_json_fetch_close.py
+++ examples/szengine/export_json_fetch_close.py
@@ -12,13 +12,11 @@ try:
     sz_abstract_factory = SzAbstractFactoryGrpc(grpc_channel)
     sz_engine = sz_abstract_factory.create_engine()
     export_handle = sz_engine.export_json_entity_report(FLAGS)
-    RESULT = ""
     while True:
-        FRAGMENT = sz_engine.fetch_next(export_handle)
-        if len(FRAGMENT) == 0:
+        fragment = sz_engine.fetch_next(export_handle)
+        if not fragment:
             break
-        RESULT += FRAGMENT
+        print(fragment, end="")
     sz_engine.close_export(export_handle)
-    print(f"\n{RESULT}\n")
 except SzError as err:
     print(f"\nERROR: {err}\n")
--- examples/szengine/process_redo_record.py
+++ examples/szengine/process_redo_record.py
@@ -11,9 +11,11 @@ try:
     grpc_channel = grpc.insecure_channel("localhost:8261")
     sz_abstract_factory = SzAbstractFactoryGrpc(grpc_channel)
     sz_engine = sz_abstract_factory.create_engine()
-    while sz_engine.count_redo_records() > 0:
-        REDO_RECORD = sz_engine.get_redo_record()
-        RESULT = sz_engine.process_redo_record(REDO_RECORD, FLAGS)
-        print(f"\n{RESULT}\n")
+    while True:
+        redo_record = sz_engine.get_redo_record()
+        if not redo_record:
+            break
+        result = sz_engine.process_redo_record(redo_record, FLAGS)
+        print(result)
 except SzError as err:
     print(f"\nERROR: {err}\n")
