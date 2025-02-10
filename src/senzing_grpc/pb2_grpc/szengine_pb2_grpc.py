90d7cf70cc9bb13a9fe9b04f755ca13669a02a9d
Michael Dockter <michael@dockter.com>
2025-02-10 15:56:24 -0500

    #209 Update gRPC (#210)

--- src/senzing_grpc/pb2_grpc/szengine_pb2_grpc.py
+++ src/senzing_grpc/pb2_grpc/szengine_pb2_grpc.py
@@ -5,7 +5,7 @@ import warnings
 
 from . import szengine_pb2 as szengine__pb2
 
-GRPC_GENERATED_VERSION = '1.66.2'
+GRPC_GENERATED_VERSION = '1.70.0'
 GRPC_VERSION = grpc.__version__
 _version_not_supported = False
 
